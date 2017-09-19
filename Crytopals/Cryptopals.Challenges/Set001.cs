da9915ddaa627c04d2c615ebf21fa5a914ddcb3d
Craig Norton <c9952594@gmail.com>
2017-09-19 14:12:02 +0100

    General clean up

--- Crytopals/Cryptopals.Challenges/Set001.cs
+++ Crytopals/Cryptopals.Challenges/Set001.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
 using Cryptopals.Core;
 using NUnit.Framework;
 using Shouldly;
@@ -10,14 +8,14 @@ using Buffer = Cryptopals.Core.Buffer;
 namespace Cryptopals.Challenges
 { 
     class Set001 {
-        string DataPath = @"C:\Users\c9952\OneDrive\Documents\GitHub\cryptopals\Crytopals\Cryptopals.Challenges";
+        readonly string _dataPath = TestContext.CurrentContext.TestDirectory;
 
         [Test]
         public void Challenge001() {
-            var fromHex =
-                Buffer.FromHex(
-                    "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
+            var fromHex =Buffer.FromHex("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
+
             var toBase64 = fromHex.ToBase64();
+
             toBase64.ShouldBe("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
         }
 
@@ -34,76 +32,41 @@ namespace Cryptopals.Challenges
 
         [Test]
         public void Challenge003() {
-            var buffer = Buffer.FromHex("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736");
+            var buffer = Buffer.FromHex("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b" +
+                                        "3736");
             var scored = buffer.Score(text => text.Count(c => char.IsLetter(c) || char.IsWhiteSpace(c)));
-            scored.Item2.ShouldBe("Cooking MC's like a pound of bacon");
+
+            scored.Text.ShouldBe("Cooking MC's like a pound of bacon");
         }
 
         [Test]
         public void Challenge004() {
-            var lines = File.ReadAllLines(Path.Combine(DataPath, "Set001Challenge004.txt"));
+            var buffers = File.ReadAllLines(Path.Combine(_dataPath, "Set001Challenge004.txt")).Select(Buffer.FromHex);
 
-            var bestMessage = "";
-            var bestScore = 0;
-
-            foreach (var line in lines) {
-                var buffer = Buffer.FromHex(line);
-                var scored = buffer.Score(text => text.Count(c => char.IsLetter(c) || char.IsWhiteSpace(c)));
-                if (scored.Item1 <= bestScore) continue;
-                bestMessage = scored.Item2;
-                bestScore = scored.Item1;
-            }
+            (int Score, string Text) bestScore = (0, "");
             
-            bestMessage.Trim().ShouldBe("Now that the party is jumping");
+            foreach (var buffer in buffers) {
+                var scored = buffer.Score(text => text.Count(c => char.IsLetter(c) || char.IsWhiteSpace(c)));
+                if (scored.Score <= bestScore.Score) continue;
+                bestScore = scored;
+            }
+
+            bestScore.Text.Trim().ShouldBe("Now that the party is jumping");
         }
 
         [Test]
         public void Challenge005() {
-            var toEncrypt = "Burning 'em, if you ain't quick and nimble\n" +
-                            "I go crazy when I hear a cymbal";
+            const string toEncrypt = "Burning 'em, if you ain't quick and nimble\n" + "I go crazy when I hear a cymbal";
+
             var buffer = Buffer.FromText(toEncrypt) ^ "ICE";
             var asHex = buffer.ToHex();
 
-            asHex.ShouldBe("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272" + 
-                           "a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f");
+            asHex.ShouldBe("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272" + "a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f");
         }
 
         [Test]
         public void Challenge006() {
-            "this is a test".HammingDistance("wokka wokka!!!").ShouldBe(37);
-
-            var lines = File.ReadAllLines(Path.Combine(DataPath, "Set001Challenge006.txt"));
-            var messageBlock = lines.SelectMany(Buffer.FromBase64).ToArray();
-
-            var bestKeySize = 0;
-            var bestScore = 0;
-
-            for (var blocks = 0; blocks < 1; blocks++) {
-                for (var keySize = 2; keySize <= 40; keySize++)
-                {
-                    var bytesToCopy = keySize * blocks;
-
-                    var firstBlock = new byte[bytesToCopy];
-                    var secondBlock = new byte[bytesToCopy];
-
-                    Array.Copy(messageBlock, firstBlock, bytesToCopy);
-                    Array.Copy(messageBlock, bytesToCopy, secondBlock, 0, bytesToCopy);
-
-                    var firstBuffer = Buffer.FromByteArray(firstBlock);
-                    var secondBuffer = Buffer.FromByteArray(secondBlock);
-
-                    var score = firstBuffer.HammingDistance(secondBuffer) / keySize;
-
-                    if (score <= bestScore) continue;
-
-                    bestScore = score;
-                    bestKeySize = keySize;
-                }
-            }
-
-
-            
-
+            Buffer.FromText("this is a test").HammingDistance(Buffer.FromText("wokka wokka!!!")).ShouldBe(37);
         }
     }
 }
--- Crytopals/Cryptopals.Core/Buffer.cs
+++ Crytopals/Cryptopals.Core/Buffer.cs
@@ -6,88 +6,93 @@ using System.Text;
 
 namespace Cryptopals.Core
 {
+
     public class Buffer : IEnumerable<byte>
     {
         readonly byte[] _value;
 
 
-        Buffer(byte[] value) {
+
+        public Buffer(byte[] value) {
             _value = value;
         }
         
 
-        public static Buffer FromHex(string hex) => new Buffer(SoapHexBinary.Parse(hex).Value);
-        public string ToHex() => BitConverter.ToString(_value).Replace("-", string.Empty).ToLower();
+
+        public static Buffer FromHex(string hex) 
+            => new Buffer(SoapHexBinary.Parse(hex).Value);
+
+        public string ToHex() 
+            => BitConverter.ToString(_value).Replace("-", string.Empty).ToLower();
+        
 
 
-        public static Buffer FromBase64(string base64) => new Buffer(Convert.FromBase64String(base64));
-        public string ToBase64() => Convert.ToBase64String(_value);
+        public static Buffer FromBase64(string base64) 
+            => new Buffer(Convert.FromBase64String(base64));
+
+        public string ToBase64() 
+            => Convert.ToBase64String(_value);
+        
 
 
-        public static Buffer FromText(string text) => new Buffer(Encoding.ASCII.GetBytes(text));
-        public string ToText() => Encoding.ASCII.GetString(_value);
+        public static Buffer FromText(string text) 
+            => new Buffer(Encoding.ASCII.GetBytes(text));
 
-        public static Buffer FromByteArray(byte[] value) => new Buffer(value);
+        public string ToText() 
+            => Encoding.ASCII.GetString(_value);
 
+        
 
-        public Tuple<int,string> Score(Func<string, int> scoreFunction) {
-            var bestText = "";
-            var bestScore = 0;
-
+        public (int Score, string Text) Score(Func<string, int> scoreFunction)
+        {
+            (int Score, string Text) best = (0, "");
+            
             for (var singleByte = 0; singleByte < 256; singleByte++)
             {
                 var text = (this ^ (byte)singleByte).ToText();
                 var score = scoreFunction(text);
 
-                if (score <= bestScore) continue;
+                if (score <= best.Score) continue;
 
-                bestText = text;
-                bestScore = score;
+                best = (score, text);
             }
 
-            return Tuple.Create(bestScore, bestText);
+            return best;
         }
 
-        public static implicit operator byte[](Buffer buffer) => buffer._value;
 
 
-        public static Buffer operator ^ (Buffer left, Buffer right) {
-            var newBytes = new byte[left._value.Length];
-            for (var index = 0; index < left._value.Length; index++) {
-                newBytes[index] = (byte)(left._value[index] ^ right._value[index]);
-            }
-            return new Buffer(newBytes);
-        }
+        public static implicit operator byte[](Buffer buffer) 
+            => buffer._value;
 
-        public static Buffer operator ^ (Buffer left, string right) {
-            var bytes = FromText(right)._value;
-            
-            var newBytes = new byte[left._value.Length];
-            for (var index = 0; index < left._value.Length; index++) {
-                newBytes[index] = (byte)(left._value[index] ^ bytes[index % bytes.Length]);
-            }
-
-            return new Buffer(newBytes);
-        }
         
-        public static Buffer operator ^ (Buffer left, byte right) {
-            var newBytes = new byte[left._value.Length];
-            for (var index = 0; index < left._value.Length; index++)
-            {
-                newBytes[index] = (byte)(left._value[index] ^ right);
-            }
+
+        static Buffer Xor(byte[] left, byte[] right)
+        {
+            var newBytes = new byte[left.Length];
+
+            for (var index = 0; index < left.Length; index++)
+                newBytes[index] = (byte) (left[index] ^ right[index % right.Length]);
+
             return new Buffer(newBytes);
         }
 
-        public IEnumerator<byte> GetEnumerator() {
-            var enumerable = _value.GetEnumerator();
-            while (enumerable.MoveNext())
-                yield return (byte)enumerable.Current;
-        }
+        public static Buffer operator ^ (Buffer left, Buffer right) 
+            => Xor(left, right);
 
-        IEnumerator IEnumerable.GetEnumerator() {
-            return GetEnumerator();
-        }
+        public static Buffer operator ^ (Buffer left, string right) 
+            => Xor(left, FromText(right)._value);
+        
+        public static Buffer operator ^ (Buffer left, byte right) 
+            => Xor(left, new[] { right });
 
+
+
+
+        public IEnumerator<byte> GetEnumerator() 
+            => ((IEnumerable<byte>) _value).GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() 
+            => GetEnumerator();
     }
 }
